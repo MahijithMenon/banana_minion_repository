18cc30bf37aa372d673ae42754d55301a8f91275
Mahijith <menon.mahijith@gmail.com>
2022-09-07 16:17:44 +0530

    fix output

--- script.js
+++ script.js
@@ -1,20 +1,25 @@
 var btn=document.querySelector('.btn');
-console.log(btn);
 var txtInput=document.querySelector('.txtareainput');
-var txtOutput=document.querySelector('.txtarearesult');
+var outputDiv = document.querySelector("#output");
 var serverURL="https://api.funtranslations.com/translate/minion.json";
 
 function getTranslationURL(text){
     return serverURL +"?"+"text="+text;
 }
+function errorHandler(error) {
+    console.log("error occured", error);
+    alert("something wrong with server! try again after some time")
+}
 function clickHandler(){
     var input=txtInput.value;
+    
 
     fetch(getTranslationURL(input))
         .then(response=>response.json())
         .then(json =>{
             var translated =json.contents.translated
-            txtOutput.innerText=translated;
+            outputDiv.innerText = translated;
         })   
+        .catch(errorHandler)
 };
 btn.addEventListener("click",clickHandler())
